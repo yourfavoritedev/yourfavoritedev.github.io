3aa33634abeca8c19808ec6120baa2ca79788cc5
yourfavoritedev <ChristopherNgo94@gmail.com>
2018-06-14 01:40:38 -0700

    Version 1.0 Commit
    Adding JQuery to help animate some css on the top navigation pane.

---
+++ alpha.js
@@ -0,0 +1,10 @@
+$("li a").on("mouseover", function(){
+	$("li a").toggleClass("fadeOut")
+	$(this).toggleClass("fadeOut")
+})
+
+
+$("li a").on("mouseout", function(){
+	$("li a").toggleClass("fadeOut")
+	$(this).toggleClass("fadeOut")
+})
