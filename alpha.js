e1a807310d8095096c4a06ba58f854cc192c2d3d
yourfavoritedev <ChristopherNgo94@gmail.com>
2018-06-14 15:43:34 -0700

    Version 1.0 Commit
    Made a few cosmetic updates.
    
    HTML: adjusted columns in closingBanner to prevent text from overlapping.
    
    CSS: Remove bounce effect that occurs on ios apps which reveal whitespaces when overscrolled.
    
    JS: Used JQuery for animation. When a link within a group of links is hovered, make all links fadeOut except for the focused link.

--- alpha.js
+++ alpha.js
@@ -6,3 +6,13 @@ $("#closingBanner .navBtn").on("mouseover", function(){
 $("#closingBanner .navBtn").on("mouseout", function(){
 	$("#closingBanner .navBtn").removeClass("fadeOut")
 })
+
+
+$(".mediaTable img").on("mouseover", function(){
+	$(".mediaTable img").addClass("fadeOut")
+	$(this).removeClass("fadeOut")
+})
+
+$(".mediaTable img").on("mouseout", function(){
+	$(".mediaTable img").removeClass("fadeOut")
+})
